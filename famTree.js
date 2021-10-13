dc98d87d57eead419e68f3229d0c8f8a034189b2
Heather Paterson <heather.paterson@sky.uk>
2021-10-13 10:13:22 +0100

    updated family tree

--- famTree.js
+++ famTree.js
@@ -1,44 +1,35 @@
-const firstGen = [
-    {
-        name: "Queen Elizabeth",
-        parents: [],
-               
-    
-},
-    {
-        name: "King George VI",
-        parents: [],
-      
-    
-},
 
-];
+class ftConstruct {
+    constructor (name, parent1, parent2) {
+        this.name = name;
+        this.parents = [parent1, parent2];
 
-const secGen = [
-    {
-        name: "Queen Elizabeth II",
-        parents: [firstGen[0].name, firstGen[1].name],
-        childOf: firstGen[0].name + " & " + firstGen[1].name,
+    }
+    childOf(){
         
-    },
-    {
-        name: "Princess Margaret",
-        parents: [firstGen[0].name, firstGen[1].name],
-        childOf: firstGen[0].name + " & " + firstGen[1].name,
-    },
-    {
-        name: "Prince Phillip",
-        parents: [],
+        if (typeof this.parents[0] != 'undefined'){ //if parent1 and parent2 have been defined when creating new object then this function will return the following statement
+            return this.name + ' is a child of ' + this.parents.join(' & ');
+        }
+        else{
+            return "Parents have not been defined" //if an object has been created but has not been given defined parents then this function will return this statement
+        }
         
-    
-},
-];
+    }
+}
 
-const thirdGen = [
-    {
-        name: "Diana",
-    },
-    {},
-    {},
+const firstGen = [ //first generation array of objects
+    new ftConstruct("King George VI"),
+    new ftConstruct("Queen Elizabeth")
 ];
-
+const secGen = [ //second generation array of objects
+    new ftConstruct("Queen Elizabeth II", firstGen[0].name, firstGen[1].name),
+    new ftConstruct("Princess Margaret", firstGen[0].name, firstGen[1].name),
+    new ftConstruct("Prince Philip")
+];
+const thirdGen = [//third generation array of objects
+    new ftConstruct("Prince Charles", secGen[0].name, secGen[2].name),
+    new ftConstruct("Princess Anne", secGen[0].name, secGen[2].name),
+    new ftConstruct("Prince Andrew", secGen[0].name, secGen[2].name),
+    new ftConstruct("Prince Edward", secGen[0].name, secGen[2].name),
+];
+console.log(thirdGen[2].childOf());
